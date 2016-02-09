a140d738550336b8fd9ad08c82f6c4e588cdd08a
sbaird02 <sbaird02@mail.bbk.ac.uk>
2016-02-09 17:56:06 +0000

    Tidy up

--- src/session5/composite/HtmlElement.java
+++ src/session5/composite/HtmlElement.java
@@ -3,23 +3,21 @@ package session5.composite;
 public class HtmlElement extends HtmlTag {
 
 	private String tagBody;
-	
+
 	public HtmlElement(String tagName) {
-		
+
 		this.tagName = tagName;
 	}
 
 	public void setTagBody(String tagBody) {
 		this.tagBody = tagBody;
 	}
-	
+
 	@Override
 	public void generateHtml() {
-		
+
 		System.out.print(startTag);
-		if (tagBody != null) {
-			System.out.print(tagBody);
-		}
+		System.out.print(tagBody == null ? "" : tagBody);
 		System.out.println(endTag);
 	}
 }
